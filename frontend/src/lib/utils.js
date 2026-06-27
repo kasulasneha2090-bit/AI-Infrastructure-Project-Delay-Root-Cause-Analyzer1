87007561704e1e459cfba4c00b6e9c0c8d211bf0
Sandesh DK <sandydk2005@gmail.com>
2026-06-27 15:52:20 +0530

    fix: remove unused imports in utils.js

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,6 +1,4 @@
 import { clsx } from "clsx";
-import { bgGradient } from "tailwind-merge"; // Just standard tailwind-merge
-import { PureComponent } from "react";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs) {
